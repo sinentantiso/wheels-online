3600dd24fad55d383dcfde72cda01f9c1f36f226
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-11-03 23:00:56 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  const name = "Sinethemba";
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Hello {name}!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
